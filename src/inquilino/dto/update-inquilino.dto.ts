ee43af07fc88d52bfa7f32349f2d5485f734e170
Joaquin D Delgado <158462640+JDamianDelgado@users.noreply.github.com>
2025-04-16 08:44:50 -0300

    Delete src/inquilino directory

--- src/inquilino/dto/update-inquilino.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInquilinoDto } from './create-inquilino.dto';
-
-export class UpdateInquilinoDto extends PartialType(CreateInquilinoDto) {}
