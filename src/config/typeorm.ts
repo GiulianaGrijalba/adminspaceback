78de7036295e1026638309c18fe5f2e70d0def94
Agata <agata.morales92@gmail.com>
2025-04-18 13:19:24 -0300

    cambio de parametros en env

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -13,11 +13,12 @@ console.log('DB_PASSWORD type:', typeof process.env.DB_PASSWORD);
 // Usa configuración hardcoded mientras solucionamos el problema de variables de entorno
 const config = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   entities: [join(__dirname, '..', '**', '*.entity{.ts,.js}')],
   migrations: [join(__dirname, '..', '**', '*.migration{.ts,.js}')],
   autoLoadEntities: true,
